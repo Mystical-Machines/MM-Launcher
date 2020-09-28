1acadb5218a0acfa2f3959e1e9b73d8c70af9a51
Brend Smits <brend_smits@hotmail.com>
2020-09-28 22:38:20 +0200

    Update Electron

--- index.js
+++ index.js
@@ -115,7 +115,7 @@ function createWindow() {
       nodeIntegration: true,
       contextIsolation: false,
       enableRemoteModule: true,
-      worldSafeExecuteJavaScript: false,
+      worldSafeExecuteJavaScript: true,
     },
     backgroundColor: "#171614",
   });
